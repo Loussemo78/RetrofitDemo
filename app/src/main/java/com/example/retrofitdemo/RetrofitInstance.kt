9c4e0be2ca6bcb8a6eb63c4ecab96c5efabadb5f
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-04-19 16:29:24 +0200

    Okhhtp client example added

--- app/src/main/java/com/example/retrofitdemo/RetrofitInstance.kt
+++ app/src/main/java/com/example/retrofitdemo/RetrofitInstance.kt
@@ -1,17 +1,30 @@
 package com.example.retrofitdemo
 
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInstance {
-    companion object{
-        val BASE_URL = "https://jsonplaceholder.typicode.com/"
-        fun getRetrofitInstance(): Retrofit{
+
+    companion object {
+        val interceptor = HttpLoggingInterceptor().apply {
+            this.level = HttpLoggingInterceptor.Level.BODY
+        }
+        val client = OkHttpClient.Builder().apply {
+            this.addInterceptor(interceptor)
+        }.build()
+
+        val BASE_URL = "https://jsonplaceholder.typicode.com"
+        fun getRetrofitInstance(): Retrofit {
             return Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
                 .build()
         }
+
     }
+
 }
