946bec85691b09e80e47b17092587b0d26b8c15b
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-04-19 16:01:25 +0200

    data displayed on activity

--- app/src/main/java/com/example/retrofitdemo/MainActivity.kt
+++ app/src/main/java/com/example/retrofitdemo/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.retrofitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import com.example.retrofitdemo.databinding.ActivityMainBinding
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val retService = RetrofitInstance
+            .getRetrofitInstance()
+            .create(AlbumService::class.java)
+        val responseLivedata:LiveData<Response<Albums>> = liveData {
+            val response = retService.getAlbums()
+            emit(response)
+        }
+        responseLivedata.observe(this, Observer{
+            val albumsList = it.body()?.listIterator()
+            if (albumsList != null){
+                while (albumsList.hasNext()){
+                    val albumsItem = albumsList.next()
+                    val result = " "+"Album Title : ${albumsItem.title}"+"\n"+
+                    " "+"Album id : ${albumsItem.title}"+"\n"+
+                    " "+"User Id : ${albumsItem.userId}"+"\n\n\n"
+                    binding.textView.append(result)
+                }
+            }
+        })
+
     }
 }
--- app/src/main/java/com/example/retrofitdemo/RetrofitInstance.kt
+++ app/src/main/java/com/example/retrofitdemo/RetrofitInstance.kt
@@ -4,7 +4,7 @@ import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class Retrofitinstance {
+class RetrofitInstance {
     companion object{
         val BASE_URL = "https://jsonplaceholder.typicode.com/"
         fun getRetrofitInstance(): Retrofit{
